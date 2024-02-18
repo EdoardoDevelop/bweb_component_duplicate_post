897595bc6a2f9225fe25d5d0dc3044044f0016ee
Bweb <34779521+EdoardoDevelop@users.noreply.github.com>
2024-02-18 22:55:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * ID: duplicate_post
+ * Name: Duplica post/page
+ * Description: Duplica pagine o crea template
+ * Icon: dashicons-admin-page
+ * Version: 1.1
+ * 
+ */
+require plugin_dir_path( __FILE__ ) ."inc/settings.php";
